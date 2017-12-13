4e0a5819992809260a425e37a69b9bf2e58f167c
mr.Shu <mr@shu.io>
2017-12-13 15:13:25 +0100

    update: Added Babel + reused StringField
    * Adding Babel
    
    * Reused StringField instead of TextField
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- eprihlaska/__init__.py
+++ eprihlaska/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import *
 from flask_wtf.csrf import CSRFProtect
+from flask_babel import Babel
 
 nav = Nav()
 
@@ -20,5 +21,11 @@ app.config.from_object('config')
 Bootstrap(app)
 nav.init_app(app)
 csrf.init_app(app)
+babel = Babel(app)
+app.config['BABEL_DEFAULT_LOCALE'] = 'sk'
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['sk', 'en'])
 
 from eprihlaska import views
