f8fd182dc4bbc0a13ec911ff6fd09199611ca24f
mr.Shu <mr@shu.io>
2018-01-03 20:22:12 +0100

    update: Fix password length
    Signed-off-by: mr.Shu <mr@shu.io>

--- eprihlaska/models.py
+++ eprihlaska/models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(50))
+    password = db.Column(db.String(80))
     registered_at = db.Column(db.DateTime, default=datetime.datetime.now)
 
 class ApplicationForm(db.Model):
