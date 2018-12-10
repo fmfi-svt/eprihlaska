a61162544e082aabc5a5b8cc315e23deecea033c
mr.Shu <mr@shu.io>
2018-12-10 16:09:44 +0100

    update: Ensure app form ID starts at 1300
    * Make sure the `application_form` IDs start at 1300.
    
    * Fixes #135
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- eprihlaska/models.py
+++ eprihlaska/models.py
@@ -5,12 +5,14 @@ import datetime
 import time
 from .consts import ApplicationStates
 
+
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
     registered_at = db.Column(db.DateTime, default=datetime.datetime.now)
+
 
 def application_form_change_author():
     remote_user = request.environ.get('REMOTE_USER')
@@ -19,9 +21,11 @@ def application_form_change_author():
     else:
         return 'user'
 
+
 class ApplicationForm(db.Model):
     __tablename__ = 'application_form'
-    id = db.Column(db.Integer, primary_key=True)
+    __table_args__ = {'sqlite_autoincrement': True}
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', backref='user')
     application = db.Column(db.Text)
@@ -34,11 +38,13 @@ class ApplicationForm(db.Model):
     state = db.Column(db.Enum(ApplicationStates),
                       default=ApplicationStates.in_progress)
 
+
 class ForgottenPasswordToken(db.Model):
     hash = db.Column(db.String(36), primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     valid = db.Column(db.Boolean, default=True)
     valid_until = db.Column(db.DateTime)
+
 
 class TokenModel(db.Model):
     __tablename__ = 'connect'
