8f5764fff6e1c45d865af9561019ff10c90a9758
Your Name <you@example.com>
2020-01-23 11:29:10 +0100

    trying to solve config mess

---
+++ config-sample.py
@@ -0,0 +1,38 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://eprihlaska:xxxxxxxxxxxxxxxxx@localhost/eprihlaska'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+OAUTH_CLIENT_CACHE_TYPE = 'simple'
+AUTHLIB_CACHE_TYPE = 'simple'
+
+GOOGLE_CLIENT_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+GOOGLE_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxxxx'
+
+FACEBOOK_CLIENT_KEY = 'xxxxxxxxxxxxxx'
+FACEBOOK_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxx'
+
+MAIL_SERVER = 'mailcheck.uniba.sk'
+MAIL_PORT = 25
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
+MAIL_DEFAULT_SENDER = ('ePrihlaska', 'xxxxxxxxxxxxxxxxxxxxx')
+
+SESSION_TYPE = 'filesystem'
+
+ADMINS = ['xxxxxxxxxxxxxxx', 'xxxxxxxxxxxxxxxxxx', 'xxxxxxxxxxxxxxxx']
+ERROR_EMAIL_SERVER = 'mailcheck.uniba.sk'
+ERROR_EMAIL_FROM = 'xxxxxxxxxxxxxxx'
+ERROR_EMAIL_HEADER = 'ePrihlaska - error'
+
+UA_CODE = 'UA-23362538-7'
+
+COSIGN_PROXY_DIR = '/opt/cosign/proxy'
+
+SUBMISSIONS_OPEN = True
+
+UPLOADED_RECEIPTS_DEST = 'receipts'
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
